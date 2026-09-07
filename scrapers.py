b8899e8ba2f5f0d99e5f0dbc21202ab3458040e4
mihneacaragea-pixel <mihnea.caragea@gmail.com>
2026-09-07 23:50:49 +0300

    Update scrapers.py

--- scrapers.py
+++ scrapers.py
@@ -31,7 +31,7 @@ HEADERS = {
         "image/avif,image/webp,*/*;q=0.8"
     ),
     "Accept-Language": "ro-RO,ro;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Encoding": "gzip, deflate"
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
@@ -41,8 +41,6 @@ HEADERS = {
     "Cache-Control": "max-age=0",
 }
 
-# Folosim o singura sesiune (pastreaza cookie-urile intre cereri, ca un
-# browser normal) in loc de cereri complet independente.
 _session = requests.Session()
 _session.headers.update(HEADERS)
 _visited_homepages = set()
