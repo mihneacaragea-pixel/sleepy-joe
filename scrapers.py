848082b14b77a45a4c83edc448c712e993a1e071
mihneacaragea-pixel <mihnea.caragea@gmail.com>
2026-09-07 23:48:24 +0300

    Update scrapers.py

--- scrapers.py
+++ scrapers.py
@@ -31,7 +31,7 @@ HEADERS = {
         "image/avif,image/webp,*/*;q=0.8"
     ),
     "Accept-Language": "ro-RO,ro;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate"
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
